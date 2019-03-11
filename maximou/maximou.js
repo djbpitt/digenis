9305f0ea8a7b767b6aa9af71feae08b8a2716e20
David J Birnbaum <djbpitt@gmail.com>
2019-03-11 14:57:53 -0700

    highlighting working within plectogram, not yet within reading texts

--- maximou/maximou.js
+++ maximou/maximou.js
@@ -7,8 +7,11 @@ function init() {
     // radio buttons toggle texts and plectogram
     var radios = document.getElementById('radios');
     radios.addEventListener('change', plectogramToggle, false);
-    // do stuff
-    process_harmony();
+    // plectogram text elements respond to click
+    var texts = document.querySelectorAll('g > text');
+    for (var i = 0, length = texts.length; i < length; i++) {
+        texts[i].addEventListener('click', itemToggle, false);
+    }
 }
 function controlToggle() {
     // Show/hide for configuration menu
@@ -51,26 +54,52 @@ function plectogramToggle() {
         }
     }
 }
-
-function process_harmony() {
-    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseXML
-    // Load harmony.xml to assign event listeners and behaviors
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '../harmony.xml', true);
-    
-    // If specified, responseType must be empty string or "document"
-    xhr.responseType = 'document';
-    
-    // overrideMimeType() can be used to force the response to be parsed as XML
-    xhr.overrideMimeType('text/xml');
-    
-    xhr.onload = function () {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.status === 200) {
-                // console.log(xhr.responseXML);
-            }
+function itemToggle() {
+    // grey out all text and line elements in plectogram]
+    var texts = document.querySelectorAll('g > text[id]');
+    for (var i = 0, length = texts.length; i < length; i++) {
+        texts[i].setAttribute('opacity', '.3');
+        texts[i].setAttribute('fill', 'black');
+        texts[i].setAttribute('stroke', 'black');
+    }
+    var lines = document.querySelectorAll('g > line');
+    for (var i = 0, length = lines.length; i < length; i++) {
+        lines[i].setAttribute('opacity', '.3');
+        lines[i].setAttribute('fill', 'black');
+        lines[i].setAttribute('stroke', 'black');
+    }
+    // items to highlight
+    var classes = this.getAttribute('class').split(' ');
+    for (var i = 0, length = classes.length; i < length; i++) {
+        var highlights = document.getElementsByClassName(classes[i]);
+        for (var j = 0, length = highlights.length; j < length; j++) {
+            highlights[j].setAttribute('opacity', '1');
+            highlights[j].setAttribute('fill', '#A00000');
+            highlights[j].setAttribute('stroke', '#A00000');
         }
-    };
-    
-    xhr.send(null);
+    }
 }
+
+
+/*function process_harmony() {
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseXML
+// Load harmony.xml to assign event listeners and behaviors
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '../harmony.xml', true);
+
+// If specified, responseType must be empty string or "document"
+xhr.responseType = 'document';
+
+// overrideMimeType() can be used to force the response to be parsed as XML
+xhr.overrideMimeType('text/xml');
+
+xhr.onload = function () {
+if (xhr.readyState === xhr.DONE) {
+if (xhr.status === 200) {
+// console.log(xhr.responseXML);
+}
+}
+};
+
+xhr.send(null);
+}*/
