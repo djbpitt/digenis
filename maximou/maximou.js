d6cdc9c8f34aaa52d6943720e2d22814a112ed6d
David J Birnbaum <djbpitt@gmail.com>
2019-03-16 14:49:59 -0700

    update harmony for maximou

--- maximou/maximou.js
+++ maximou/maximou.js
@@ -1,21 +1,23 @@
 window.addEventListener('DOMContentLoaded', init, false);
 function init() {
     // configuration menu responds to mouseover
-    var control = document.getElementById('controls');
+    const control = document.getElementById('controls');
     control.addEventListener('mouseover', controlToggle, true);
     control.addEventListener('mouseout', controlToggle, true);
     // radio buttons toggle texts and plectogram
-    var radios = document.getElementById('radios');
+    const radios = document.getElementById('radios');
     radios.addEventListener('change', plectogramToggle, false);
     // plectogram text elements respond to click
-    var texts = document.querySelectorAll('g > text');
-    for (var i = 0, length = texts.length; i < length; i++) {
+    const texts = document.querySelectorAll('g > text');
+    let i = 0;
+    const length = texts.length;
+    for (; i < length; i++) {
         texts[i].addEventListener('click', itemToggle, false);
     }
 }
 function controlToggle() {
     // Show/hide for configuration menu
-    var controls = document.querySelector('#controls > div');
+    const controls = document.querySelector('#controls > div');
     if (controls.style.display !== 'block') {
         controls.style.display = 'block';
     } else {
@@ -25,54 +27,54 @@ function controlToggle() {
 function plectogramToggle() {
     // Swap ae ~ gc plectogram and texts; value will be "ae" (Slavic) or "gc" (Greek)
     // First plectograms
-    radio = document.querySelector('input[name="plect_choice"]:checked').value;
-    var plectograms = document.querySelectorAll('#plectogram > div');
-    for (var i = 0, length = plectograms.length; i < length; i++) {
+    const radio = document.querySelector('input[name="plect_choice"]:checked').value;
+    const plectograms = document.querySelectorAll('#plectogram > div');
+    for (let i = 0, length = plectograms.length; i < length; i++) {
         plectograms[i].style.display = 'none';
     }
     document.getElementById(radio).style.display = 'block';
     // Then texts
-    var slavic_texts = document.getElementsByClassName('slavic');
+    const slavic_texts = document.getElementsByClassName('slavic');
     // ae
-    var greek_texts = document.getElementsByClassName('greek');
+    const greek_texts = document.getElementsByClassName('greek');
     // gc
-    if (radio == 'ae') {
+    if (radio === 'ae') {
         // show Slavic (a, e) and hide Greek (c, g)
-        for (var i = 0, length = greek_texts.length; i < length; i++) {
+        for (let i = 0, length = greek_texts.length; i < length; i++) {
             greek_texts[i].style.display = 'none';
         }
-        for (var i = 0, length = slavic_texts.length; i < length; i++) {
+        for (let i = 0, length = slavic_texts.length; i < length; i++) {
             slavic_texts[i].style.display = 'block';
         }
     } else {
         // show Greek (c, g) and hide Slavic (a, e)
-        for (var i = 0, length = slavic_texts.length; i < length; i++) {
+        for (let i = 0, length = slavic_texts.length; i < length; i++) {
             slavic_texts[i].style.display = 'none';
         }
-        for (var i = 0, length = greek_texts.length; i < length; i++) {
+        for (let i = 0, length = greek_texts.length; i < length; i++) {
             greek_texts[i].style.display = 'block';
         }
     }
 }
 function itemToggle() {
     // grey out all text and line elements in plectogram
-    var texts = document.querySelectorAll('g > text[id]');
-    for (var i = 0, length = texts.length; i < length; i++) {
+    const texts = document.querySelectorAll('g > text[id]');
+    for (let i = 0, length = texts.length; i < length; i++) {
         texts[i].setAttribute('opacity', '.3');
         texts[i].setAttribute('fill', 'black');
         texts[i].setAttribute('stroke', 'black');
     }
-    var lines = document.querySelectorAll('g > line');
-    for (var i = 0, length = lines.length; i < length; i++) {
+    const lines = document.querySelectorAll('g > line');
+    for (let i = 0, length = lines.length; i < length; i++) {
         lines[i].setAttribute('opacity', '.3');
         lines[i].setAttribute('fill', 'black');
         lines[i].setAttribute('stroke', 'black');
     }
     // highlight inside plectogram
-    var classes = this.getAttribute('class').split(' ');
-    for (var i = 0, length = classes.length; i < length; i++) {
-        var highlights = document.getElementsByClassName(classes[i]);
-        for (var j = 0, length = highlights.length; j < length; j++) {
+    const classes = this.getAttribute('class').split(' ');
+    for (let i = 0, length = classes.length; i < length; i++) {
+        const highlights = document.getElementsByClassName(classes[i]);
+        for (let j = 0, length = highlights.length; j < length; j++) {
             highlights[j].setAttribute('opacity', '1');
             highlights[j].setAttribute('fill', '#A00000');
             highlights[j].setAttribute('stroke', '#A00000');
