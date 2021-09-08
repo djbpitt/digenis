8cee430ec21ccda8922413a3cae021352bdf0166
David J Birnbaum <djbpitt@gmail.com>
2021-09-08 09:16:39 -0400

    Create script to generate all reading views (process-all.sh)wq

---
+++ reader/process-all.sh
@@ -0,0 +1,6 @@
+for f in *-annotated.xml
+do
+    outfile=$(echo $f | sed -e 's/-annotated.xml/.xhtml/')
+    echo "Processing $f"
+    java -jar /opt/saxon/he/saxon-he-10.1.jar -s:$f -xsl:create-reading-view.xsl -o:$outfile
+done
