a86a3df11692aff149ed41d73a7f54fbece54d1d
djbpitt <djbpitt@pitt.edu>
2021-09-09 16:57:34 -0400

    Update processing script for Saxon 10.5

--- reader/process-all.sh
+++ reader/process-all.sh
@@ -2,5 +2,5 @@ for f in *-annotated.xml
 do
     outfile=$(echo $f | sed -e 's/-annotated.xml/.xhtml/')
     echo "Processing $f"
-    java -jar /opt/saxon/he/saxon-he-10.1.jar -s:$f -xsl:create-reading-view.xsl -o:$outfile
+    java -jar /opt/saxon/he/saxon-he-10.5.jar -s:$f -xsl:create-reading-view.xsl -o:$outfile
 done
