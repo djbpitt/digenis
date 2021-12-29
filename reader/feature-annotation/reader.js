de239dca536faec96476dd2ef60bb5740cec44b7
David J Birnbaum <djbpitt@gmail.com>
2021-12-28 21:08:09 -0500

    Annotation popups are now css, not js

--- reader/feature-annotation/reader.js
+++ reader/feature-annotation/reader.js
@@ -1,22 +1,15 @@
 "use strict";
+/*
+ * Word-level annotations at bottom are controlled by js
+ * Popup in-place annotations and fly-out menu overlay are pure css
+ */
 window.addEventListener('DOMContentLoaded', function () {
-  const annotations = document.getElementsByClassName('note');
-  for (var i = 0, len = annotations.length; i < len; i++) {
-    annotations[i].addEventListener('mouseover', overlay, false);
-    annotations[i].addEventListener('mouseout', clear, false);
-  }
   const ws = document.getElementsByClassName('w');
   for (var i = 0, len = ws.length; i < len; i++) {
     ws[i].addEventListener('mouseover', grammar, false);
   }
 },
 false)
-function overlay(event) {
-  this.getElementsByClassName('annotation')[0].style.visibility = 'visible';
-}
-function clear(event) {
-  this.getElementsByClassName('annotation')[0].style.visibility = 'hidden';
-}
 function grammar(event) {
   hide_grammars();
   document.getElementById('grammar-' + this.firstElementChild.innerText).style.display = 'block';
